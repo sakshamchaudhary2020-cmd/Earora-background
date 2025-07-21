3f700112c434a5340da40095add66af325e89368
sakshamchaudhary2020-cmd <sakshamchaudhary2020@gmail.com>
2025-07-21 11:27:31 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+app.post('/api/send-location', async (req, res) => {
+  const { name, lat, lon } = req.body;
+
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.GMAIL_USER,
+      pass: process.env.GMAIL_PASS
+    }
+  });
+
+  const mailOptions = {
+    from: 'Earora <' + process.env.GMAIL_USER + '>',
+    to: 'sakshamchaudhary2020@gmail.com',
+    subject: 'New Visitor on Earora',
+    text: `Name: ${name}\nLatitude: ${lat}\nLongitude: ${lon}`
+  };
+
+  try {
+    await transporter.sendMail(mailOptions);
+    res.status(200).json({ message: 'Location sent!' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Error sending email' });
+  }
+});
+
+app.listen(3000, () => {
+  console.log('Server running on http://localhost:3000');
+});
